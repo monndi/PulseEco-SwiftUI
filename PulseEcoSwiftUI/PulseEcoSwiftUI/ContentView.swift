3c6c50b388bbc57f7da2256c3a23ea1a865e1a23
Monika Dimitrova <Monika.Dimitrova@netcetera.com>
2020-05-28 13:04:20 +0200

    Initial commit

---
+++ PulseEcoSwiftUI/PulseEcoSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PulseEcoSwiftUI
+//
+//  Created by Monika Dimitrova on 5/28/20.
+//  Copyright © 2020 Monika Dimitrova. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
